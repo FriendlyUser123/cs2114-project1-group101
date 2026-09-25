8184e63f5f391b5915d57e080b50864214c54ee3
nandiniduggaraju <nandini.duggaraju@gmail.com>
2026-09-25 10:26:18 -0400

    added the evaluatePolynomial() method

--- src/DerivativeCalculator.java
+++ src/DerivativeCalculator.java
@@ -1,17 +1,52 @@
 package calculator;
-/**
-Evaluates a polynomial and its derivative at a specific x value
 
-@author
-@version Sep 25, 2026
-*/
+/**
+ * Evaluates a polynomial and its derivative at a specific x value
+ * 
+ * @author
+ * @version Sep 25, 2026
+ */
 
 public class DerivativeCalculator
 {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
 
-    //~ Constructors ..........................................................
+    // ~ Constructors ..........................................................
 
-    //~Public  Methods ........................................................
+    // ~Public Methods ........................................................
+    /**
+     * Solves a polynomial at a given x value
+     * 
+     * @param poly
+     *            the polynomial to solve
+     * @param x
+     *            the double value to solve at
+     * @return double the solution
+     * @throw IllegalArgumentException when the input is bad
+     * @throw ArithmeticException when the polynomial couldn't evaluate
+     */
+    public double evaluatePolynomial(Polynomial poly, double x)
+    {
+        if (poly == null)
+        {
+            throw new IllegalArgumentException("Polynomial can't be null");
+        }
+        if (Double.isNaN(x) || Double.isInfinite(x))
+        {
+            throw new IllegalArgumentException("Invalid x value.");
+        }
+        double[] coefficients = poly.toCoefficientArray();
+        double result = 0.0;
+        for (int i = 0; i < coefficients.length; i++)
+        {
+            result += coefficients[i] * Math.pow(x, i);
+        }
+        if (Double.isNaN(result) || Double.isInfinite(result))
+        {
+            throw new ArithmeticException(
+                "Evaluation resulted in an invalid value.");
+        }
+        return result;
+    }
 
 }
