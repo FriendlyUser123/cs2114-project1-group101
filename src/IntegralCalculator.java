5182277c318a86b8c19a98dccff40e5d7645f74e
nandiniduggaraju <nandini.duggaraju@gmail.com>
2026-09-25 11:13:22 -0400

    added the simpsonsRule() method

--- src/IntegralCalculator.java
+++ src/IntegralCalculator.java
@@ -1,18 +1,68 @@
 package calculator;
 
 /**
-Solves integrals numerically with the power rule and Simpson's rule.
-
-@author
-@version Sep 25, 2026
-*/
+ * Solves integrals numerically with the power rule and Simpson's rule.
+ * 
+ * @author
+ * @version Sep 25, 2026
+ */
 
 public class IntegralCalculator
 {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
 
-    //~ Constructors ..........................................................
+    // ~ Constructors ..........................................................
 
-    //~Public  Methods ........................................................
+    // ~Public Methods ........................................................
+    /**
+    Approximates the definite integral of a polynomial
+    between two x values using Simpson's Rule.
+
+    @param poly polynomial to integrate
+    @param lowerBound lower x bound
+    @param upperBound upper x bound
+    @param intervals number of subintervals
+    @return approximate definite integral
+    */
+    public double simpsonsRule(Polynomial poly, double lowerBound,
+    double upperBound, int intervals)
+    {
+        if (poly == null)
+        {
+            throw new IllegalArgumentException("Polynomial cannot be null");
+        }
+        if (Double.isNaN(lowerBound) || Double.isInfinite(lowerBound)
+        || Double.isNaN(upperBound) || Double.isInfinite(upperBound))
+        {
+            throw new IllegalArgumentException("Bounds must be finite");
+        }
+        if (intervals <= 0 || intervals % 2 != 0)
+        {
+            throw new IllegalArgumentException(
+            "Number of intervals must be positive and even");
+        }
+        double h = (upperBound - lowerBound) / intervals;
+        double result = evaluatePolynomial(poly, lowerBound) + evaluatePolynomial(poly, upperBound);
+        for (int i = 1; i < intervals; i++)
+        {
+            double x = lowerBound + i * h;
+            double y = evaluatePolynomial(poly, x);
+            if (i % 2 == 0)
+            {
+                result += 2.0 * y;
+            }
+            else
+            {
+                result += 4.0 * y;
+            }
+        }
+        result = h / 3.0;
+        if (Double.isNaN(result) || Double.isInfinite(result))
+        {
+            throw new ArithmeticException(
+            "Integral resulted in an invalid value.");
+        }
+        return result;
+    }
 
 }
