05e3f047dc6d6ffc1cd6921cd859c202225017aa
nandiniduggaraju <nandini.duggaraju@gmail.com>
2026-09-25 10:20:07 -0400

    derivative calculator class
    will take polynomials and find their derivatives

---
+++ src/DerivativeCalculator.java
@@ -0,0 +1,17 @@
+package calculator;
+/**
+Evaluates a polynomial and its derivative at a specific x value
+
+@author
+@version Sep 25, 2026
+*/
+
+public class DerivativeCalculator
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
