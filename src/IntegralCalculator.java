91a8c3999536bee2a1fb6f603391eb8baa4fe3ea
nandiniduggaraju <nandini.duggaraju@gmail.com>
2026-09-25 11:07:51 -0400

    created IntegralCalculator class

---
+++ src/IntegralCalculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+/**
+Solves integrals numerically with the power rule and Simpson's rule.
+
+@author
+@version Sep 25, 2026
+*/
+
+public class IntegralCalculator
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
